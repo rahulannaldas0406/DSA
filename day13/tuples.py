03b2bb3b26d03b3bd44c59a50b42ac7d0b4ef243
rahulannaldas0406 <rahulannaldas2006@gmail.com>
2025-07-17 12:19:53 +0530

    collection of data types

--- day13/tuples.py
+++ day13/tuples.py
@@ -1,6 +1,78 @@
-tuple=()
+'''tuple=()
 print(tuple)
 tuple=("rahul",1,4,3.3,"arjun")
 print(tuple)
 li=["godavari",3,2,1]
-print(tuple(li))
+print(tuple(li))'''
+
+'''tuple=(1,2,3,4,5)
+print(tuple)'''
+
+'''tuple=('rahul','sindhu','fahad','sneha','nazyria')
+
+a,b,c,c,b=tuple
+print(a)
+print(b)
+print(c)
+print(c)
+print(b)'''
+
+
+'''student_info = ("Rahul", 20, (85, 90, 78))
+
+name,age,marks=student_info
+print("name:",name)
+print("age:",age)
+print("marks:",marks)
+
+name,age,marks=student_info
+a,b,c=marks
+print(b)'''
+
+
+'''my_tuple=(1,2,3,4,5)
+for item in my_tuple:
+    if item==5:
+        print("yes")
+if 5 in my_tuple:
+    print("yes")'''
+
+'''tup1 = (1, 2)
+tup2 = (3, 4)
+print(tup1+tup2)'''
+
+# my_tuple=(1,2,3,4)
+# my_tuple=()
+# for i in my_list:
+#     my_tuple.append(i)
+
+#my_tuple=tuple(my_list)
+#print(tuple(my_list))
+
+# print(min(my_tuple))
+# print(max(my_tuple))
+'''from array import array
+
+my_array = array('i', [1, 2, 3, 4])  # 'i' = type code for integer
+my_array.append(5)
+print(my_array)'''
+
+#<-----list problems----->
+
+'''lst = [10, 20, 30, 40]
+
+lst.insert(2,22)
+lst.remove(10)
+lst.sort()
+print(lst)'''
+
+#<----Dictionary---->
+
+dict={"name":"rahul","age":18}
+# for i in dict:
+#     if i in "name":
+#         print("yes")
+
+print(dict.values())
+
+

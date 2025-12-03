cb594742a9bc2fc30039b7242354bbcb28153b49
Annaldas Rahul <rahulannaldas2006@gmail.com>
2025-12-03 22:56:35 +0530

    Update rescursion

--- Function/day51/Function.py
+++ Function/day51/Function.py
@@ -1,2 +1,23 @@
 #Find the power of any number using recursion.
 
+'''def name(n):
+    if n==0:
+        return 
+    print("Rahul")
+    return name(n-1)
+name(5)'''
+
+
+'''def number(a,n):
+    if n==0:
+        return 
+    print(a)
+    return number(a+1,n-1)
+number(1,5)'''
+
+'''def num(n):
+    if n==0:
+        return 
+    print(n)
+    return num(n-1)
+num(5)'''

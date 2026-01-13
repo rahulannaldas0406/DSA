e571213c5128255260ef22d28d8cc689cdb6d246
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-01-13 22:07:38 +0530

    Update code

--- TECHNIQUES/Twopointer.py
+++ TECHNIQUES/Twopointer.py
@@ -162,9 +162,9 @@ print(a)'''
 
 '''Example: nums=[3,2,2,3],val=3 output:2,nums=[2,2,_,_]'''
 
-arr=[3,2,2,3]
+'''arr=[3,2,2,3]
 val=3
-for i in range(0,len(arr)):
+for i in range(0,len(arr)):'''
     
 
 

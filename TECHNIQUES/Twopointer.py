b2b5eae4b8890e5a387a4bb52fb0abeb88f748b8
Annaldas Rahul <rahulannaldas2006@gmail.com>
2026-01-25 23:12:35 +0530

    Update code

--- TECHNIQUES/Twopointer.py
+++ TECHNIQUES/Twopointer.py
@@ -176,7 +176,9 @@ for i in range(0,len(arr)):'''
 2️⃣ 3Sum (if time)'''
 
 
-'''container problem will take time beacuse didn't understanding the problem '''
+'''container problem will take time beacuse didn't understanding the problem  b '''
+
+#Break for mids!
 
 
 
